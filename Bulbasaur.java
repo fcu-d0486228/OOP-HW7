35d9d92757d12c1ed0c84bee96c3432db7d5ae07
fcu-d0486228 <d0486228@fcu.edu.tw>
2016-12-16 08:45:35 +0800

    first commit

---
+++ Bulbasaur.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon {
+
+	public Bulbasaur(String name, PokemonType type, int cp) {
+		super(name, type, cp);
+	}
+	
+	@Override
+	public void attack() {
+		System.out.println("Tackle¡K");
+	}
+}
---
+++ Psyduck.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Psyduck extends Pokemon {
+	
+	public Psyduck(String name, PokemonType type, int cp) {
+		super(name, type, cp);
+	}
+	
+	@Override
+	public void attack() {
+		System.out.println("Aqua Tail¡K");
+	}
+}
